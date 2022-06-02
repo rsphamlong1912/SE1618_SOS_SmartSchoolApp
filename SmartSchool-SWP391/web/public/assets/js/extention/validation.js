dce5816a776116452141e05a8d73f2cc2833c5bc
Baro <ngbao1592001@gmail.com>
2022-06-02 21:33:42 +0700

    Set up Frontend

---
+++ SmartSchool-SWP391/web/public/assets/js/extention/validation.js
@@ -0,0 +1,69 @@
+var fullName = document.querySelector('#fullName')
+var username = document.querySelector('#userName')
+var password = document.querySelector('#password')
+var repassword = document.querySelector('#re-password')
+var email = document.querySelector('#email')
+var phone = document.querySelector('#phone')
+var form = document.querySelector('form')
+
+
+function showError(input, message){
+    let parent = input.parentElement;
+    let small = parent.querySelector('small')
+    small.innerText = message
+}
+
+function showSuccess(input){
+    let parent = input.parentElement;
+    let small = parent.querySelector('small')
+    small.innerText = ''
+}
+
+
+function checkEmptyError(listInput){
+    let isEmptyError = false;
+    listInput.forEach(input =>{
+        input.value = input.value.trim()
+
+        if(!input.value){
+            isEmptyError = true
+            showError(input, 'Không được để trống !')
+        }else {
+            showSuccess(input)
+        }
+    });
+    return isEmptyError
+}
+
+function checkEmailError(input){
+    const regexEmail =
+     /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+     input.value = input.value.trim()
+     let isEmailError = !regexEmail.test(input.value)
+     if(regexEmail.test(input.value)){
+         showSuccess(input)
+     }else {
+         showError(input, 'Email không hợp lệ !')
+     }
+
+     return isEmailError
+}
+
+function checkLengthError(input, min, max){
+    input.value = input.value.trim()
+
+    if(input.value.length < min){
+        showError(input, 'Phải có ít nhất ${min} ký tự !')
+        return true
+    }
+    if(input.value.length > max){
+        showError(input, 'Không quá ${max} ký tự !')
+    }
+    showSuccess(input)
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    let isEmptyError = checkEmptyError([fullName, username, password, repassword, email, phone])
+    let isEmailError = checkEmailError(email)
+})
