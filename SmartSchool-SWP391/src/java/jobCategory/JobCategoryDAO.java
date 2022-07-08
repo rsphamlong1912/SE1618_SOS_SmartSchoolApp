88936bacec9ea17353d2d75407afb87a841cb962
Nguyen Van Hai Nam <nguyenhainam10102001@gmail.com>
2022-07-08 22:38:19 +0700

    CRUD JobCategory

--- SmartSchool-SWP391/src/java/jobCategory/JobCategoryDAO.java
+++ SmartSchool-SWP391/src/java/jobCategory/JobCategoryDAO.java
@@ -1,11 +1,14 @@
 package jobCategory;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import utills.DBUtils;
 
 /**
@@ -15,7 +18,9 @@ import utills.DBUtils;
 public class JobCategoryDAO {
 
     private static final String LISTALL_CATEGORY = "select * from tblCategoryJob where statusCategory=1";
-
+    private static final String CREATE = "INSERT INTO tblCategoryJob(statusCategory, jobCategoryName, jobImage, jobDescription) VALUES(1,?,?,?)";
+    private static final String DELETE = "UPDATE tblCategoryJob SET statusCategory=0 WHERE jobCategoryId=?";
+    private static final String UPDATE = "UPDATE tblCategoryJob SET jobCategoryName=?, jobImage=?, jobDescription=? WHERE jobCategoryId=?";
     public List<JobCategoryDTO> getAllCategory() throws SQLException {
         List<JobCategoryDTO> list = new ArrayList<>();
         Connection con = null;
@@ -50,5 +55,72 @@ public class JobCategoryDAO {
             }
         }
         return list;
+    }
+    
+    public void updateJobCategory( int jobCategoryId,String jobCategoryName,String jobImage,String jobDescription ) throws SQLException {
+        Connection con = null;
+        PreparedStatement ptm = null;
+        try {
+            con = DBUtils.getConnection();
+            ptm = con.prepareStatement(UPDATE);
+            ptm.setString(1,jobCategoryName);
+            ptm.setString(2, jobImage);
+            ptm.setString(3, jobDescription);
+            ptm.setInt(4, jobCategoryId);
+            ptm.executeUpdate();
+        } catch (Exception ex) {
+            Logger.getLogger(JobCategoryDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+
+            if (ptm != null) {
+                ptm.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+    
+    public void deleteJobCategory(int jobCategoryId) throws SQLException {
+        Connection con = null;
+        PreparedStatement stm = null;
+        try {
+            con = DBUtils.getConnection();
+            stm = con.prepareStatement(DELETE);
+            stm.setInt(1, jobCategoryId);
+            stm.executeUpdate();
+        } catch (Exception ex) {
+            Logger.getLogger(JobCategoryDTO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (stm != null) {
+                stm.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+    
+     public void uploadJobCategory(String jobCategoryName, String jobImage, String jobDescription ) throws SQLException {
+        Connection con = null;
+        PreparedStatement ptm = null;
+        try {
+            con = DBUtils.getConnection();
+            ptm = con.prepareStatement(CREATE);
+            ptm.setString(1, jobCategoryName);
+            ptm.setString(2, jobImage);
+            ptm.setString(3, jobDescription);
+            ptm.executeUpdate();
+        } catch (Exception ex) {
+            Logger.getLogger(JobCategoryDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+
+            if (ptm != null) {
+                ptm.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
     }
 }
